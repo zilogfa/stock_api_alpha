eceaeb776d7f6d0fa243c08721558f91544880b1
zilogfa <Zilogfa@live.com>
2023-12-12 09:20:41 -0800

    more Charts added.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template, jsonify, send_from_directory
 import requests
 import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg')  # Matplotlib backend to 'Agg'
 import matplotlib.pyplot as plt
+
 import os
 import io
 import base64
@@ -81,6 +85,92 @@ def fetch_stock_data_with_stats(symbol, api_key):
     else:
         return None, None
     
+def calculate_stats(df):
+    stats = {
+        'mean': df['4. close'].mean(),
+        'max': df['4. close'].max(),
+        'min': df['4. close'].min(),
+        'std_dev': df['4. close'].std(),  # Standard deviation
+        'latest': df['4. close'].iloc[-1]
+        # You can add more statistics here as needed
+    }
+    return stats
+
+
+# Line Chart for Daily Closing Prices
+def plot_closing_prices(df, symbol):
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.plot(df.index, df['4. close'], label='Closing Price', color='blue')
+    ax.set_title(f'{symbol} Closing Prices')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.grid(True)
+    ax.legend()
+    return fig
+
+
+# Bar Chart for Trading Volume
+def plot_trading_volume(df, symbol):
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.bar(df.index, df['5. volume'], color='orange')
+    ax.set_title(f'{symbol} Trading Volume')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Volume')
+    ax.grid(True)
+    return fig
+
+
+# Moving Average
+def plot_moving_average(df, symbol, window_size=20):
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.plot(df.index, df['4. close'], label='Closing Price', color='blue', alpha=0.5)
+    ax.plot(df.index, df['4. close'].rolling(window=window_size).mean(), label=f'{window_size}-Day Moving Average', color='red')
+    ax.set_title(f'{symbol} Moving Average')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.grid(True)
+    ax.legend()
+    return fig
+
+
+# Histogram of Daily Price Changes
+
+def plot_daily_price_change_histogram(df, symbol):
+    fig, ax = plt.subplots(figsize=(10, 4))
+    daily_changes = df['4. close'].pct_change().dropna()
+    ax.hist(daily_changes, bins=50, color='green', alpha=0.7)
+    ax.set_title(f'{symbol} Daily Price Changes')
+    ax.set_xlabel('Percentage Change')
+    ax.set_ylabel('Frequency')
+    ax.grid(True)
+    return fig
+
+
+
+# Saving Plots as Images
+def create_plots(df, symbol):
+    plot_functions = [plot_closing_prices, plot_trading_volume, plot_moving_average, plot_daily_price_change_histogram]
+    urls = []
+
+    for plot_func in plot_functions:
+        fig = plot_func(df, symbol)
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png', bbox_inches='tight')
+        plt.close(fig)
+        buf.seek(0)
+        plot_url = base64.b64encode(buf.getvalue()).decode('utf-8')
+        urls.append(f"data:image/png;base64,{plot_url}")
+
+    return urls
+
+
+
+
+
+
+
+
+
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -93,18 +183,24 @@ def index():
     return render_template('index.html')
 
 
-
 @app.route('/get-stock-data', methods=['POST'])
 def stock_data():
-    symbol = request.form['symbol']
-    api_key = get_api_key()
-    df, stats = fetch_stock_data_with_stats(symbol, api_key)
-    if df is not None:
-        plot_url = create_plot(df, symbol)
-        return jsonify({'plot_url': plot_url, 'stats': stats})
-    else:
-        return jsonify({'error': 'Failed; invalid symbol or exceeded API rate limit'})
-    
+    try:
+        symbol = request.form['symbol']
+        api_key = get_api_key()
+        df = fetch_stock_data(symbol, api_key)
+        if df is not None:
+            print("Dataframe fetched successfully")  # Debugging
+            plot_urls = create_plots(df, symbol)
+            stats = calculate_stats(df)
+            print("Plots and stats generated")  # Debugging
+            return jsonify({'plot_urls': plot_urls, 'stats': stats})
+        else:
+            print("Failed to fetch data")  # Debugging
+            return jsonify({'error': 'Failed; invalid symbol or exceeded API rate limit'})
+    except Exception as e:
+        print("An error occurred:", e)
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,41 @@
 $(document).ready(function(){
     $('#stockForm').submit(function(e){
         e.preventDefault();  // Prevent default form submission
-        console.log('Form submitted'); // Debugging line
+        console.log('Form submitted'); 
 
         $.ajax({
             url: '/get-stock-data',
             type: 'post',
             data: $('#stockForm').serialize(),
             success: function(data) {
-                console.log('AJAX request succeeded'); // Debugging line
-                $('#result').empty(); // Clear previous results
-                if (data.plot_url) {
+                console.log('AJAX request succeeded');
+                $('#result').empty(); 
+                
+                if (data.plot_urls) {  // Changed from plot_url to plot_urls
                     if (data.stats) {
                         $('#result').append(`
-                                            <div class="static">
-                                            <p><span>Mean:</span> <span>${data.stats.mean}</span></p>
-                                            <p><span>Max:</span> <span>${data.stats.max}</span></p>
-                                            <p><span>Min:</span> <span>${data.stats.min}</span></p>
-                                            <div>`);
-                        // Append other stats as needed
+                            <div class="static">
+                                <p><span>Mean:</span> <span>${data.stats.mean}</span></p>
+                                <p><span>Max:</span> <span>${data.stats.max}</span></p>
+                                <p><span>Min:</span> <span>${data.stats.min}</span></p>
+                                <p><span>std_dev:</span> <span>${data.stats.std_dev}</span></p>
+                                <p><span>Latest:</span> <span>${data.stats.latest}</span></p>
+                            <div>`);
                     }
-                    $('#result').append(`<img src="${data.plot_url}" alt="Stock Data"/>`);
+
+                    data.plot_urls.forEach(url => {
+                        $('#result').append(`<img src="${url}" alt="Stock Chart" style="margin-top: 20px;"/>`);
+                    });
                     
                 } else {
                     $('#result').append(`<p>Error: ${data.error}</p>`);
                 }
             },
-            error: function() {
-                console.log('AJAX request failed'); // Debugging line
-                $('#result').html('<p>An error occurred while processing your request.</p>');
+            error: function(xhr, status, errorThrown) {
+                console.log('AJAX error status:', status);
+                console.log('Thrown Error:', errorThrown);
+                console.log('Response:', xhr.responseText);
+                $('#result').html(`<p>Error: ${xhr.responseText || 'Unknown error'}</p>`);
             }
         });
     });
